2f7fe74ad56bbb114548a053fee7ae75f28725c2
gbolahanO <gbolahanolawuyi4u@gmail.com>
2021-06-22 22:48:36 +0100

    refactored code to use modern react

--- src/Components/Store.js
+++ src/Components/Store.js
@@ -1,23 +1,14 @@
 import { applyMiddleware, createStore, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from '../reducers'
+import rootReducer from "../reducers";
 
-
-// const store = createStore(() => [], {}, applyMiddleware());
-
-// const initialState = {};
-
-// const middleware = [compose];
-
-// const store = createStore(
-//   rootReducer,
-// //   initialState,
-//   compose(applyMiddleware(thunk))
-// );
-
+// setup configuration for React Dev Tools chrome extension
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(thunk))
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,16 +1,16 @@
-import { FETCH_POSTS, NEW_POST } from "./types";
+import { NEW_POST, LOADING } from "./types";
+
+export const loading = () => ({ type: LOADING });
+
+export const newPosts = (data) => ({ type: NEW_POST, payload: data });
 
 // export function fetchPosts(){}
-export const fetchPosts = () => (dispatch) => {  // going to dispatch fetchPosts to the reducer 
-  return function (dispatch) {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((posts) =>
-        dispatch({  
-          type: FETCH_POSTS,
-          payload: posts, // data thats coming into the reducer, we call payload
-        })
-      );
-  };
+export const fetchPosts = () => (dispatch) => {
+  // going to dispatch fetchPosts to the reducer
+  dispatch(loading());
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((posts) => {
+      dispatch(newPosts(posts));
+    });
 };
-
